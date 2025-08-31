014fbc1bb841be19c690c7ec5acdc16bca7a9b50
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-09-01 01:24:03 +0300

    add benchmark project for testing performance and memory usage

---
+++ ProblemSolving/ProblemSolving.Benchmarks/LeetCode/Array-String/BestTimeBuySellStock121Benchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using ProblemSolving.LeetCode.Array_String;
+
+namespace ProblemSolving.Benchmarks.LeetCode.Array_String
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class BestTimeBuySellStock121Benchmarks
+    {
+        private int[] _prices = [];
+        private readonly BestTimeBuySellStock121 _pricingService = new();
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _prices = GenerateTestData(100000);
+        }
+
+        [Benchmark]
+        public int MaxProfit_Benchmark() => _pricingService.MaxProfit(_prices);
+
+        private int[] GenerateTestData(int size)
+        {
+            var random = new Random(42); // Seed for consistency
+            return Enumerable.Range(1, size)
+                           .Select(_ => random.Next(1, 10000))
+                           .ToArray();
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving.Benchmarks/Program.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Running;
+using ProblemSolving.Benchmarks.LeetCode.Array_String;
+
+//BenchmarkRunner.Run(typeof(Program).Assembly);
+BenchmarkRunner.Run<BestTimeBuySellStock121Benchmarks>();
+
+Console.ReadLine();
