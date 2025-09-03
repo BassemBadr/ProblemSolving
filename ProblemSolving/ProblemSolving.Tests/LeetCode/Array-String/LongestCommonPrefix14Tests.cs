0f369def198d8502c16c075b3a5f37c8ad1e6171
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-09-04 00:32:57 +0300

    dd solution for problem 14 longest common prefix tried some solutions and i think i will settle with solution 2

---
+++ ProblemSolving/ProblemSolving.Tests/LeetCode/Array-String/LongestCommonPrefix14Tests.cs
@@ -0,0 +1,266 @@
+﻿using ProblemSolving.LeetCode.Array_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Tests.LeetCode.Array_String
+{
+    [TestFixture]
+    public class LongestCommonPrefix14Tests
+    {
+        private LongestCommonPrefix14 _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new LongestCommonPrefix14();
+        }
+
+        [Test]
+        [Description("Example 1 from problem description")]
+        public void LongestCommonPrefix_Example1_ShouldReturnfl()
+        {
+            //  Arrange
+            string[] strs = { "flower", "flow", "flight" };
+            string expected = "fl";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("Example 2 from problem description")]
+        public void LongestCommonPrefix_Example2_ShouldReturnEmpty()
+        {
+            //  Arrange
+            string[] strs = { "dog", "racecar", "car" };
+            string expected = "";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("all strings are the same word")]
+        public void LongestCommonPrefix_AllSameWord_ShouldReturnWord()
+        {
+            //  Arrange
+            string[] strs = { "word", "word", "word", "word", "word", "word", "word", "word" };
+            string expected = "word";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("all strings are the same word but some are empty")]
+        public void LongestCommonPrefix_AllSameWordWithEmpty_ShouldReturnEmpty()
+        {
+            //  Arrange
+            string[] strs = { "word", "word", "word", "word", "", "word", "word", "word" };
+            string expected = "";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("first letter common")]
+        public void LongestCommonPrefix_FirstLetterCommon_ShouldReturnW()
+        {
+            //  Arrange
+            string[] strs = { "word", "wood", "ward", "wand", "wild", "wane", "war", "wizard" };
+            string expected = "w";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("multiple common one letter")]
+        public void LongestCommonPrefix_CommonOneLetter_ShouldReturnO()
+        {
+            //  Arrange
+            string[] strs = { "o", "o", "o", "o" };
+            string expected = "o";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("single letter")]
+        public void LongestCommonPrefix_SingleLetter_ShouldReturnl()
+        {
+            //  Arrange
+            string[] strs = { "l" };
+            string expected = "l";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("first 3 letters are common")]
+        public void LongestCommonPrefix_3CommonLetters_ShouldReturnOne()
+        {
+            //  Arrange
+            string[] strs = { "one", "oneday", "onenight", "onegashima", "oneda" };
+            string expected = "one";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("string array has one item")]
+        public void LongestCommonPrefix_OneWordArray_ShouldReturnParty()
+        {
+            //  Arrange
+            string[] strs = { "party" };
+            string expected = "party";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("string array has one empty item")]
+        public void LongestCommonPrefix_EmptyItem_ShouldReturnEmpty()
+        {
+            //  Arrange
+            string[] strs = { "" };
+            string expected = "";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("boundary with 200 character words")]
+        public void LongestCommonPrefix_200CharWords_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            string[] strs = { new string('c', 200), new string('c', 100) + new string('b', 100), new string('c', 50) + new string('b', 150), new string('c', 25) + new string('b', 175) };
+            string expected = new string('c', 25);
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("test boudnary with array length of 200 word")]
+        public void LongestCommonPrefix_200ItemsArray_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            string[] strs = new string[200];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                strs[i] = new string('a', i + 1);
+            }
+
+            string expected = "a";
+
+            //  Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void LongestCommonPrefix_AllEmptyStrings_ReturnsEmptyString()
+        {
+            // Arrange
+            string[] strs = { "", "", "" };
+            string expected = "";
+
+            // Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void LongestCommonPrefix_MaximumStringLength_HandlesLongStrings()
+        {
+            // Arrange
+            string longPrefix = new string('a', 100); // 100 'a' characters
+            string[] strs = { longPrefix + "bc", longPrefix + "de", longPrefix + "fg" };
+            string expected = longPrefix;
+
+            // Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void LongestCommonPrefix_MaximumStringLengthNoCommonPrefix_ReturnsEmpty()
+        {
+            // Arrange
+            string str1 = new string('a', 200);
+            string str2 = new string('b', 200);
+            string[] strs = { str1, str2 };
+            string expected = "";
+
+            // Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void LongestCommonPrefix_AllSameLongString_ReturnsEntireLongString()
+        {
+            // Arrange
+            string longString = new string('z', 150);
+            string[] strs = { longString, longString, longString };
+            string expected = longString;
+
+            // Act
+            string result = _solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/LeetCode/Array-String/LongestCommonPrefix14.cs
@@ -0,0 +1,71 @@
+﻿namespace ProblemSolving.LeetCode.Array_String
+{
+    public class LongestCommonPrefix14
+    {
+        //https://leetcode.com/problems/longest-common-prefix
+        public string LongestCommonPrefix(string[] strs)
+        {
+            return Solution2(strs);
+        }
+
+        private static string Solution3(string[] strs)
+        {
+            //  time should be O(nlog n) and space of O(m)
+            //  this is actually worse
+            Array.Sort(strs);
+            int minLength = Math.Min(strs[0].Length, strs[strs.Length - 1].Length);
+            int index = 0;
+            while (index < minLength && strs[0][index] == strs[strs.Length - 1][index])
+                index++;
+            string commonPrefix = strs[0].Substring(0, index);
+            return commonPrefix;
+        }
+
+        private static string Solution2(string[] strs)
+        {
+            //  this looks alittle better in readability and some operations are saved although not by much 
+            //  overall still time complexity O(n*m) and space O(m) where m is longest prefix
+            string commonPrefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                int minLength = Math.Min(commonPrefix.Length, strs[i].Length);
+                int index = 0;
+
+                while (index < minLength && commonPrefix[index] == strs[i][index])
+                {
+                    index++;
+                }
+                commonPrefix = commonPrefix.Substring(0, index);
+
+                if (commonPrefix.Length == 0)
+                    break;
+            }
+            return commonPrefix;
+        }
+
+        private static string Solution1(string[] strs)
+        {
+            //  the idea is to assume the common prefix is the entire first word 
+            //  then cut down from it as i check more words 
+            //  time complexity O(n*m) and space O(m) where m is longest prefix
+            string commonPrefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                int minLength = Math.Min(commonPrefix.Length, strs[i].Length);
+                if (minLength < commonPrefix.Length)
+                    commonPrefix = commonPrefix.Substring(0, minLength);
+                for (int index = 0; index < minLength; index++)
+                {
+                    if (commonPrefix[index] != strs[i][index])
+                    {
+                        commonPrefix = commonPrefix.Substring(0, index);
+                        break;
+                    }
+                }
+                if (commonPrefix.Length == 0)
+                    break;
+            }
+            return commonPrefix;
+        }
+    }
+}
