957bf969bcfac275a74e070d2729efa3d5ac96d6
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-09-10 01:55:16 +0300

    add solution for word pattern problem

---
+++ ProblemSolving/ProblemSolving.Tests/LeetCode/Hashmap/WordPattern290Tests.cs
@@ -0,0 +1,177 @@
+﻿using ProblemSolving.LeetCode.Hashmap;
+
+namespace ProblemSolving.Tests.LeetCode.Hashmap
+{
+    [TestFixture]
+    public class WordPattern290Tests
+    {
+        private WordPattern290 _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new WordPattern290();
+        }
+
+        [Test]
+        [Description("Example 1 from problem description 'abba' & 'dog cat cat dog'")]
+        public void WordPattern_Example1_ShouldReturnTrue()
+        {
+            //  Arrange
+            string pattern = "abba";
+            string s = "dog cat cat dog";
+
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [Description("Example 2 from problem description 'abba' & 'dog cat cat fish'")]
+        public void WordPattern_Example2_ShouldReturnFalse()
+        {
+            //  Arrange
+            string pattern = "abba";
+            string s = "dog cat cat fish";
+
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        [Description("Example 3 from problem description 'aaaa' & 'dog cat cat dog'")]
+        public void WordPattern_Example3_ShouldReturnFalse()
+        {
+            //  Arrange
+            string pattern = "aaaa";
+            string s = "dog cat cat dog";
+
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        [Description("basic positive cases")]
+        [TestCase("abba", "dog cat cat dog")]
+        [TestCase("abc", "dog cat fish")]
+        [TestCase("a", "dog")]
+        public void WordPattern_BasicPositive_ShouldReturnTrue(string pattern, string s)
+        {
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [Description("basic negative cases")]
+        [TestCase("abba", "dog cat cat fish")]
+        [TestCase("aaaa", "dog cat cat dog")]
+        [TestCase("abba", "dog dog dog dog")]
+        public void WordPattern_BasicNegative_ShouldReturnFalse(string pattern, string s)
+        {
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        [Description("length mismatch")]
+        [TestCase("abc", "dog cat")]
+        [TestCase("ab", "dog cat fish")]
+        [TestCase("", "dog cat")]
+        [TestCase("abc", "")]
+        public void WordPattern_LengthMismatch_ShouldReturnFalse(string pattern, string s)
+        {
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        [Description("Bijection Validation Cases")]
+        [TestCase("abba", "dog dog dog dog")]
+        [TestCase("aaaa", "dog cat cat dog")]
+        [TestCase("abba", "dog cat fish dog")]
+        public void WordPattern_Bijection_ShouldReturnFalse(string pattern, string s)
+        {
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        [Description("Single Character word cases")]
+        [TestCase("a", "dog", true)]
+        [TestCase("aa", "dog dog", true)]
+        [TestCase("aa", "dog cat", false)]
+        [TestCase("ab", "dog dog", false)]
+        public void WordPattern_Bijection_ShouldReturnWorkCorrectly(string pattern, string s, bool expected)
+        {
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("complex pattern")]
+        [TestCase("abcabc", "dog cat fish dog cat fish", true)]
+        [TestCase("abcabc", "dog cat fish dog cat bird", false)]
+        [TestCase("aabbcc", "dog dog cat cat fish fish", true)]
+        public void WordPattern_Complex_ShouldReturnWorkCorrectly(string pattern, string s, bool expected)
+        {
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("boundary case correct")]
+        public void WordPattern_BoundaryCase_ShouldReturnTrue()
+        {
+            //  Arrange
+            string pattern = new string('a', 300);
+            string s = string.Join(" ", Enumerable.Repeat("word", 300));
+
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [Description("boundary case incorrect")]
+        public void WordPattern_BoundaryCase_ShouldReturnFalse()
+        {
+            //  Arrange
+            string pattern = new string('a', 300);
+            string s = string.Join(" ", Enumerable.Repeat("word", 299));
+
+            //  Act
+            bool result = _solution.WordPattern(pattern, s);
+
+            //  Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/LeetCode/Hashmap/WordPattern290.cs
@@ -0,0 +1,111 @@
+﻿namespace ProblemSolving.LeetCode.Hashmap
+{
+    public class WordPattern290
+    {
+        //https://leetcode.com/problems/word-pattern
+        public bool WordPattern(string pattern, string s)
+        {
+            //  the idea is to reverse it and loop on words of string s
+            //  keep tracking of mapping in both ways
+            //  time complexity of O(n)
+            //  space of O(p + w)
+            //  still don't like it i think there is still much better / cleaner solution
+            string[] charMapping = new string[256];
+            Dictionary<string, char> wordMapping = [];
+            int wordCount = 0;
+            int patternIndex = -1;
+            List<char> wordChars = [];
+            for (int i = 0; i <= s.Length; i++)
+            {
+                if (i == s.Length || s[i] == ' ')
+                {
+                    patternIndex++;
+                    wordCount++;
+                    if (patternIndex >= pattern.Length)
+                        return false;
+
+                    char c = pattern[patternIndex];
+                    string newWord = string.Join("", wordChars);
+                    if (charMapping[c] == null && !wordMapping.ContainsKey(newWord))
+                    {
+                        charMapping[c] = newWord;
+                        wordMapping.Add(newWord, c);
+                    }
+                    else if ((!wordMapping.TryGetValue(newWord, out char key)) || key != c || charMapping[c] != newWord)
+                    {
+                        return false;
+                    }
+                    wordChars = [];
+                }
+                else
+                {
+                    wordChars.Add(s[i]);
+                }
+            }
+            if (wordCount != pattern.Length)
+                return false;
+
+            return true;
+        }
+
+        private static bool Solution1(string pattern, string s)
+        {
+            //  first solution to come to mind 
+            //  is to create mapping and go through the letters of pattern 
+            //  then if not exist in mapping (as key or value) add it and if exist compare with previous if not equal return false
+            //  all while keeping count of word count to compare vs pattern letters
+            //  and string length to see if it matches the pattern
+            //  time and space complexity are alittle complicated but i guess its as follows
+            //  time complexity O(n + p^2) which is really bad because of the mapping.ContainsValue(word) part
+            //  space complexity of O(p)
+
+            if (pattern.Length == 0 && s.Length != 0)
+                return false;
+
+            if (s.Length == 0 && pattern.Length != 0)
+                return false;
+
+            Dictionary<char, string> mapping = [];
+
+            int wordIndex = 0;
+            int charCount = 0;
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (wordIndex >= s.Length)
+                    break;
+
+                char c = pattern[i];
+                List<char> nextWord = [];
+                while (wordIndex < s.Length && s[wordIndex] != ' ')
+                {
+                    nextWord.Add(s[wordIndex]);
+                    wordIndex++;
+                }
+
+                string word = string.Join("", nextWord);
+                charCount++;
+                if (mapping.TryGetValue(c, out string? wordValue))
+                {
+                    if (wordValue != word)
+                        return false;
+                }
+                else if (mapping.ContainsValue(word))
+                {
+                    return false;
+                }
+                else
+                {
+                    mapping.Add(c, word);
+                }
+                wordIndex++;
+            }
+            if (wordIndex < s.Length)
+                return false;
+
+            if (charCount != pattern.Length)
+                return false;
+
+            return true;
+        }
+    }
+}
