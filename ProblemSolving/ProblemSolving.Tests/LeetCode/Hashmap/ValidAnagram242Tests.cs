a066ecad0f2b1c2fffcada96d44f84b1950b1790
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-09-10 23:53:10 +0300

    apply solution for problem 242 to check if two strings are anagrams or not and the followup

---
+++ ProblemSolving/ProblemSolving.Tests/LeetCode/Hashmap/ValidAnagram242Tests.cs
@@ -0,0 +1,151 @@
+﻿using ProblemSolving.LeetCode.Hashmap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Tests.LeetCode.Hashmap
+{
+    [TestFixture]
+    public class ValidAnagram242Tests
+    {
+        private ValidAnagram242 _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new ValidAnagram242();
+        }
+
+        [Test]
+        [Description("Example 1 from problem description 'anagram' & 'nagaram'")]
+        public void IsAnagram_Example1_ShouldReturnTrue()
+        {
+            //  Arrange
+            string s = "anagram";
+            string t = "nagaram";
+            bool expected = true;
+
+            //  Act 
+            bool result = _solution.IsAnagram(s, t);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("Example 2 from problem description 'rat' & 'car'")]
+        public void IsAnagram_Example2_ShouldReturnFalse()
+        {
+            //  Arrange
+            string s = "rat";
+            string t = "car";
+            bool expected = false;
+
+            //  Act 
+            bool result = _solution.IsAnagram(s, t);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("single letter test cases")]
+        [TestCase("a", "a", true)]
+        [TestCase("a", "b", false)]
+        public void IsAnagram_Example2_ShouldReturnFalse(string s, string t, bool expected)
+        {
+            //  Act 
+            bool result = _solution.IsAnagram(s, t);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("two letters test cases")]
+        [TestCase("aa", "aa", true)]
+        [TestCase("ab", "ab", true)]
+        [TestCase("ab", "ba", true)]
+        [TestCase("ab", "ca", false)]
+        [TestCase("ab", "ac", false)]
+        public void IsAnagram_TwoLetters_ShouldWorkCorrrectly(string s, string t, bool expected)
+        {
+            //  Act 
+            bool result = _solution.IsAnagram(s, t);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("multible letters test cases")]
+        [TestCase("abcd", "abcd", true)]
+        [TestCase("abcd", "dsba", false)]
+        [TestCase("abcd", "badc", true)]
+        [TestCase("abcd", "bacd", true)]
+        [TestCase("abcd", "abbd", false)]
+        [TestCase("aaaa", "aaaa", true)]
+        [TestCase("aabb", "baba", true)]
+        [TestCase("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz", true)]
+        [TestCase("abcdefghijklmnopqrstuvwxyz", "zabcdefghijklmnopqrstuvwxy", true)]
+        public void IsAnagram_MultipleLetters_ShouldWorkCorrrectly(string s, string t, bool expected)
+        {
+            //  Act 
+            bool result = _solution.IsAnagram(s, t);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("different length test cases")]
+        [TestCase("abcdef", "abcdef", true)]
+        [TestCase("abcdef", "abcde", false)]
+        [TestCase("abcdef", "abcdefg", false)]
+        public void IsAnagram_DifferentLength_ShouldWorkCorrrectly(string s, string t, bool expected)
+        {
+            //  Act 
+            bool result = _solution.IsAnagram(s, t);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("max length test case")]
+        public void IsAnagram_MaxLength_ShouldWorkCorrrectly()
+        {
+            //  Arrange
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string s = new string('a', 45000) + alphabet;
+            string t = alphabet + new string('a', 45000);
+            bool expected = true;
+
+            //  Act 
+            bool result = _solution.IsAnagram(s, t);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Description("max length test case")]
+        public void IsAnagram_MaxLength2_ShouldWorkCorrrectly()
+        {
+            //  Arrange
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string s = new string('a', 45000) + alphabet;
+            string t = alphabet + new string('b', 45000);
+            bool expected = false;
+
+            //  Act 
+            bool result = _solution.IsAnagram(s, t);
+
+            //  Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/LeetCode/Hashmap/ValidAnagram242.cs
@@ -0,0 +1,139 @@
+﻿namespace ProblemSolving.LeetCode.Hashmap
+{
+    public class ValidAnagram242
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            //  first if the length is not equal return false
+            //  then count unique characters from s string 
+            //  then loop on char in t string and reduce their count and if any count gone below 0 return false
+            //  otherwise then all went well return true
+            //  time complexity of O(n)
+            //  space complexity of O(1)
+            if (s.Length != t.Length)
+                return false;
+
+            int[] charCount = new int[256];
+            for (int i = 0; i < s.Length; i++)
+            {
+                charCount[s[i]]++;
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                charCount[t[i]]--;
+                if (charCount[t[i]] < 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static bool FollowUp(string s, string t)
+        {
+            //  for the followup 
+            //  would replacee the array with dictionary
+            //  time complexity O(n)
+            //  space complexity O(k) k: number of unique characters in s
+            if (s.Length != t.Length)
+                return false;
+
+            Dictionary<char, int> charCount = [];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount.ContainsKey(s[i]))
+                    charCount[s[i]]++;
+                else
+                    charCount.Add(s[i], 1);
+            }
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (!charCount.ContainsKey(t[i]))
+                    return false;
+                else
+                {
+                    charCount[t[i]]--;
+                    if (charCount[t[i]] < 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool Solution3(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            int[] charCount = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                charCount[s[i] - 'a']++;
+                charCount[t[i] - 'a']--;
+            }
+
+            foreach (int c in charCount)
+            {
+                if (c != 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static bool Solution2(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            Dictionary<char, int> charCount = [];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount.ContainsKey(s[i]))
+                    charCount[s[i]]++;
+                else
+                    charCount.Add(s[i], 1);
+
+                if (charCount.ContainsKey(t[i]))
+                    charCount[t[i]]--;
+                else
+                    charCount.Add(t[i], -1);
+            }
+
+            foreach (int c in charCount.Values)
+            {
+                if (c != 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static bool Solution1(string s, string t)
+        {
+            //  first idea is to create mapping for each character count
+            //  then loop on words and set character count
+            //  then compare final result that each character still has the same count in the two strings
+            //  time complexity of O(n)
+            //  space complexity of O(1)
+            if (s.Length != t.Length)
+                return false;
+
+            int[] sCount = new int[256];
+            int[] tCount = new int[256];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                sCount[s[i]]++;
+                tCount[t[i]]++;
+            }
+
+            for (int i = 0; i < sCount.Length; i++)
+            {
+                if (sCount[i] != tCount[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
