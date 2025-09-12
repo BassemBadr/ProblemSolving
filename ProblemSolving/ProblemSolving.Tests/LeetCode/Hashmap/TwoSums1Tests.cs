b37575c19d2e29bbef579dc66dd113002f426b59
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-09-12 03:37:23 +0300

    add solution for two sum problem

---
+++ ProblemSolving/ProblemSolving.Tests/LeetCode/Hashmap/TwoSums1Tests.cs
@@ -0,0 +1,236 @@
+﻿using ProblemSolving.LeetCode.Hashmap;
+
+namespace ProblemSolving.Tests.LeetCode.Hashmap
+{
+    [TestFixture]
+    public class TwoSums1Tests
+    {
+        private TwoSums1 _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new TwoSums1();
+        }
+
+        [Test]
+        [Description("example 1 from problem description nums = [2,7,11,15], target = 9")]
+        public void TwoSum_Example1_ShouldWorkCorrectly()
+        {
+            // Arrange
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] expected = { 0, 1 };
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(result, Is.EquivalentTo(expected));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        [Description("example 2 from problem description nums = [3,2,4], target = 6")]
+        public void TwoSum_Example2_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            int[] nums = { 3, 2, 4 };
+            int target = 6;
+            int[] expected = { 1, 2 };
+
+            //  Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            //  Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(result, Is.EquivalentTo(expected));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        [Description("example 3 from problem description nums = [3,3], target = 6")]
+        public void TwoSum_Example3_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            int[] nums = { 3, 3 };
+            int target = 6;
+            int[] expected = { 0, 1 };
+
+            //  Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            //  Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(result, Is.EquivalentTo(expected));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        public void TwoSum_MinimumArraySize_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { 1, 2 };
+            int target = 3;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        public void TwoSum_NegativeNumbers_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { -1, -2, -3, -4, -5 };
+            int target = -8;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        public void TwoSum_PositiveAndNegativeNumbers_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { -1, 2, 3, -4, 5 };
+            int target = 1;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        public void TwoSum_ZeroTarget_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { -5, 0, 5, 10 };
+            int target = 0;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        public void TwoSum_LargeNumbers_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { 1000000000, 2000000000, -1000000000, -2000000000 };
+            int target = 0;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        public void TwoSum_DuplicateValuesDifferentIndices_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 2, 4 };
+            int target = 4;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        public void TwoSum_SolutionAtEndOfArray_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int target = 19;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+            Assert.That(result, Is.EquivalentTo(new[] { 8, 9 })); // 9 + 10 = 19
+        }
+
+        [Test]
+        public void TwoSum_SolutionAtBeginningOfArray_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { 10, 20, 30, 40, 50 };
+            int target = 30;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+            Assert.That(result, Is.EquivalentTo(new[] { 0, 1 }));
+        }
+
+        [Test]
+        public void TwoSum_MinimumValues_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { -109, -109, 0, 109 };
+            int target = 0;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        public void TwoSum_MaximumValues_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { 109, 109, 0, -109 };
+            int target = 218;
+
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+
+        [Test]
+        [TestCase(new[] { 1, 2, 3 }, 4)]
+        [TestCase(new[] { 4, 5, 6 }, 11)]
+        [TestCase(new[] { 10, 15, 20 }, 35)]
+        public void TwoSum_MultipleScenarios_AllReturnCorrect(int[] nums, int target)
+        {
+            // Act
+            int[] result = _solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(nums[result[0]] + nums[result[1]], Is.EqualTo(target));
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/LeetCode/Hashmap/TwoSums1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace ProblemSolving.LeetCode.Hashmap
+{
+    public class TwoSums1
+    {
+        //https://leetcode.com/problems/two-sum
+        public int[] TwoSum(int[] nums, int target)
+        {
+            //  time O(n)
+            //  space O(n)
+            Dictionary<int, int> valueMapping = [];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int searchValue = target - nums[i];
+                if (valueMapping.TryGetValue(searchValue, out int index) && i != index)
+                {
+                    return [index, i];
+                }
+
+                if (!valueMapping.ContainsKey(nums[i]))
+                    valueMapping.Add(nums[i], i);
+            }
+            return [];
+        }
+
+        private static int[] Solution3(int[] nums, int target)
+        {
+            Dictionary<int, int> valueMapping = [];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (valueMapping.ContainsKey(target - nums[i]))
+                    valueMapping[target - nums[i]] = i;
+                else
+                    valueMapping.Add(target - nums[i], i);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (valueMapping.TryGetValue(nums[i], out int index) && i != index)
+                {
+                    return [i, index];
+                }
+            }
+            return [];
+        }
+
+        private static int[] Solution2(int[] nums, int target)
+        {
+            //  time O(n log n)
+            //  space O(n)
+            int[] sortedArray = new int[nums.Length];
+            nums.CopyTo(sortedArray, 0);
+            Array.Sort(sortedArray);
+            int num1 = 0, num2 = 0;
+            bool isNumFound = false;
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                int low = i + 1, high = sortedArray.Length - 1;
+                int targetNum2 = target - sortedArray[i];
+                while (low <= high)
+                {
+                    int mid = low + (high - low) / 2;
+                    if (targetNum2 == sortedArray[mid])
+                    {
+                        num1 = sortedArray[i];
+                        num2 = targetNum2;
+                        isNumFound = true;
+                        break;
+                    }
+                    else if (targetNum2 > sortedArray[mid])
+                    {
+                        low = mid + 1;
+                    }
+                    else if (targetNum2 < sortedArray[mid])
+                    {
+                        high = mid - 1;
+                    }
+                }
+                if (isNumFound) break;
+            }
+
+            int[] result = new int[2];
+            bool num1Found = false, num2Found = false;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == num1 && !num1Found)
+                {
+                    result[0] = i;
+                    num1Found = true;
+                }
+                else if (nums[i] == num2 && !num2Found)
+                {
+                    result[1] = i;
+                    num2Found = true;
+                }
+                if (num1Found && num2Found)
+                    break;
+            }
+
+            return result;
+        }
+
+        private static int[] Solution1(int[] nums, int target)
+        {
+            //  time O(n ^ 2)
+            //  space O(1)
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return [i, j];
+                }
+            }
+            return [];
+        }
+    }
+}
