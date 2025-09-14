f50be03ee7619f55b2dd68478210dc10255dbc85
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-09-15 00:24:53 +0300

    add solution for summary ranges 228 problem

---
+++ ProblemSolving/ProblemSolving.Tests/LeetCode/Intervals/SummaryRanges228Tests.cs
@@ -0,0 +1,142 @@
+﻿using ProblemSolving.LeetCode.Intervals;
+
+namespace ProblemSolving.Tests.LeetCode.Intervals
+{
+    [TestFixture]
+    public class SummaryRanges228Tests
+    {
+        private SummaryRanges228 _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new SummaryRanges228();
+        }
+
+        [Test]
+        public void SummaryRanges_Example1_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            int[] nums = { 0, 1, 2, 4, 5, 7 };
+            string[] expected = { "0->2", "4->5", "7" };
+
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void SummaryRanges_Example2_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            int[] nums = { 0, 2, 3, 4, 6, 8, 9 };
+            string[] expected = { "0", "2->4", "6", "8->9" };
+
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void SummaryRanges_EmptyArray_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            int[] nums = { };
+            string[] expected = { };
+
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void SummaryRanges_SingleElement_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            int[] nums = { 1 };
+            string[] expected = { "1" };
+
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void SummaryRanges_TwoConsecutive_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            int[] nums = { 4, 5 };
+            string[] expected = { "4->5" };
+
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void SummaryRanges_TwoNonConsecutive_ShouldWorkCorrectly()
+        {
+            //  Arrange
+            int[] nums = { 4, 6 };
+            string[] expected = { "4", "6" };
+
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        [TestCase(new int[] { 0, 1, 2, 4, 5, 7 }, new string[] { "0->2", "4->5", "7" })]
+        [TestCase(new int[] { 1, 3, 5, 7 }, new string[] { "1", "3", "5", "7" })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, new string[] { "1->6" })]
+        [TestCase(new int[] { -3, -2, -1, 1, 2, 4, 5 }, new string[] { "-3->-1", "1->2", "4->5" })]
+        [TestCase(new int[] { -2, -1, 0, 1, 2 }, new string[] { "-2->2" })]
+        public void SummaryRanges_NormalCases_ShouldWorkCorrectly(int[] nums, string[] expected)
+        {
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        [TestCase(new int[] { -2147483648, -2147483647, 2147483646, 2147483647 }, new string[] { "-2147483648->-2147483647", "2147483646->2147483647" })]
+        [TestCase(new int[] { 1000000, 1000001, 1000003, 1000004, 1000005 }, new string[] { "1000000->1000001", "1000003->1000005" })]
+        [TestCase(new int[] { -1, 0, 1, 2, 4 }, new string[] { "-1->2", "4" })]
+        public void SummaryRanges_BoundaryCases_ShouldWorkCorrectly(int[] nums, string[] expected)
+        {
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 4, 6, 7, 8, 10 }, new string[] { "1->2", "4", "6->8", "10" })]
+        [TestCase(new int[] { 1, 3, 4, 5, 7 }, new string[] { "1", "3->5", "7" })]
+        [TestCase(new int[] { 1, 2, 3, 5, 7 }, new string[] { "1->3", "5", "7" })]
+        [TestCase(new int[] { 1, 2, 4, 5, 7, 8 }, new string[] { "1->2", "4->5", "7->8" })]
+        public void SummaryRanges_AdditionalCases_ShouldWorkCorrectly(int[] nums, string[] expected)
+        {
+            //  Act
+            IList<string> result = _solution.SummaryRanges(nums);
+
+            //  Assert
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/LeetCode/Intervals/SummaryRanges228.cs
@@ -0,0 +1,124 @@
+﻿namespace ProblemSolving.LeetCode.Intervals
+{
+    public class SummaryRanges228
+    {
+        //https://leetcode.com/problems/summary-ranges
+        public IList<string> SummaryRanges(int[] nums)
+        {
+            //  time O(n)
+            //  space O(1)
+            IList<string> result = [];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int start = nums[i];
+                //  create loop to skip consecutive numbers
+                while (i < nums.Length - 1 && nums[i] + 1 == nums[i + 1])
+                {
+                    i++;
+                }
+                //  check if single or range
+                if (start == nums[i])
+                {
+                    result.Add($"{start}");
+                }
+                else
+                {
+                    result.Add($"{start}->{nums[i]}");
+                }
+            }
+            return result;
+        }
+
+        private static IList<string> Solution1(int[] nums)
+        {
+            //  time O(n)
+            //  space O(1)
+            //  but needs alot of cleanup
+            List<string> result = [];
+
+            if (nums.Length == 0) return result;
+            if (nums.Length == 1)
+            {
+                result.Add(nums[0].ToString());
+                return result;
+            }
+
+            //  store first element in a string
+            string firstNumber = nums[0].ToString();
+            string lastNumber = string.Empty;
+            bool isRange = false;
+
+            //  start from second element
+            for (int i = 1; i < nums.Length; i++)
+            {
+                //  if current number = previous number +1
+                if (nums[i] == nums[i - 1] + 1)
+                {
+                    //      set last number in range as current number
+                    lastNumber = nums[i].ToString();
+                    //      if is last
+                    if (i == nums.Length - 1)
+                    {
+                        //          add first number, last number as range to the result list
+                        result.Add(firstNumber + "->" + lastNumber);
+                    }
+                    //      else if not last
+                    else
+                    {
+                        //          set is range = true
+                        isRange = true;
+                        //          continue to the next number
+                        continue;
+                    }
+
+
+                }
+                //  else if current number != previous +1
+                else
+                {
+                    //      if is last
+                    if (i == nums.Length - 1)
+                    {
+                        //          if is range
+                        if (isRange)
+                        {
+                            //              add first number, last number as range to the result list
+                            result.Add(firstNumber + "->" + lastNumber);
+                        }
+                        //          else if not range
+                        else
+                        {
+                            //              add first number as single element to the result list
+                            result.Add(firstNumber);
+                        }
+                        //          add current number as single element to the result list
+                        result.Add(nums[i].ToString());
+                    }
+                    //      else if is not last
+                    else
+                    {
+                        //          if is range
+                        if (isRange)
+                        {
+                            //              add first number, last number as range to the result list
+                            result.Add(firstNumber + "->" + lastNumber);
+                        }
+                        //          else if not range
+                        else
+                        {
+                            //              add first number as single element to the result list
+                            result.Add(firstNumber);
+                        }
+                        //          set first element as current element
+                        firstNumber = nums[i].ToString();
+                        //           set last element as empty
+                        lastNumber = string.Empty;
+                        //            set is range = false
+                        isRange = false;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
